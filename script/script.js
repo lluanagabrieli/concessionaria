bb49541dc46ba465fe6b300a12a9bcdb07fa4278
Msnaider23 <00020923@fepi.edu.br>
2024-05-24 20:49:52 -0300

    feat: adiciona modal

--- script/script.js
+++ script/script.js
@@ -11,3 +11,83 @@ function abrirMenu() {
     menuAberto = false;
   }
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// PEGANDO ELEMENTOS
+var modal = document.getElementById("modal");
+var botao = document.getElementById("botao");
+
+var modal2 = document.getElementById("modal2");
+var botao2 = document.getElementById("botao2");
+
+var modal3 = document.getElementById("modal3");
+var botao3 = document.getElementById("botao3");
+
+var modal4 = document.getElementById("modal4");
+var botao4 = document.getElementById("botao4");
+
+// FECHAR MODAL
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+var span3 = document.getElementsByClassName("close")[2];
+var span4 = document.getElementsByClassName("close")[3];
+
+// MODAL
+botao.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// MODAL 2
+botao2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
+// MODAL 3
+botao3.onclick = function() {
+  modal3.style.display = "block";
+}
+
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+}
+
+// MODAL 4
+botao4.onclick = function() {
+  modal4.style.display = "block";
+}
+
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+}
